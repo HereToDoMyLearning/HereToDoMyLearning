e9f951389d262585bbf72ce444578b38299abcd7
HereToDoMyLearning <115903370+HereToDoMyLearning@users.noreply.github.com>
2022-11-29 14:38:46 +0100

    Create Begrenzer.java

---
+++ Begrenzer.java
@@ -0,0 +1,23 @@
+// HereToDoMyLearning
+
+package *;
+public class * {
+	public static void main (String[] args) {
+	int value = 5000;
+	System.out.println(ueberpruefungwert(value));
+
+	}
+
+public static int ueberpruefungwert (int value) {
+    if (value > 500) {
+        return 500;
+        // Wert wurde auf 500 angeglichen
+    } else if (value < -500) {
+        return -500;
+       // Wert wurde auf -500 angeglichen
+    } else {
+        return value;
+       // System.out.println(value);
+    }
+}
+}
